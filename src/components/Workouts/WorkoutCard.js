49f040ae1e970619fbc41c5e717e79e5a5729716
S Pickford <spickford@ss-mbp-2.dhcp.nd.edu>
2024-12-11 23:25:58 -0500

    videos

--- src/components/Workouts/WorkoutCard.js
+++ src/components/Workouts/WorkoutCard.js
@@ -10,6 +10,10 @@ const WorkoutCard = ({ workout }) => {
             <p><strong>Equipment:</strong> {workout.equipment}</p>
             <p><strong>Duration:</strong> {workout.duration} minutes</p>
             <p><strong>Calories Burned:</strong> {workout.calories_burned} calories</p>
+            <div className='video_container'>
+                <iframe src ={workout.video}></iframe>
+            </div>
+
         </div>
     );
 };
